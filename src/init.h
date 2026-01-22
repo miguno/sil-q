4db90b6cdb048a0980f33626aa4db18cc1454ec2
miguno <miguno@miguno.com>
2026-01-22 13:11:51 +0100

    fix: remove redeclarations to fix -Wredundant-decl warning

--- src/init.h
+++ src/init.h
@@ -132,7 +132,6 @@ extern header r_head;
 extern header p_head;
 extern header c_head;
 extern header h_head;
-extern header b_head;
 extern header g_head;
 extern header q_head;
 extern header n_head;
