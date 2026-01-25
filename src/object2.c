0c660ca9a9217291765110c5f1c7525f9a689c78
miguno <miguno@miguno.com>
2026-01-26 00:56:16 +0100

    Use `__attribute__ ((fallthrough))`` instead of fallthrough comment to support clang, too

--- src/object2.c
+++ src/object2.c
@@ -1277,8 +1277,8 @@ bool object_similar(const object_type* o_ptr, const object_type* j_ptr)
         if (!object_known_p(o_ptr) || !object_known_p(j_ptr))
             return (FALSE);
 
+        __attribute__((fallthrough));
     }
-    /* FALLTHRU */
 
     /* Weapons and Armor */
     case TV_BOW:
@@ -1295,7 +1295,7 @@ bool object_similar(const object_type* o_ptr, const object_type* j_ptr)
     case TV_SOFT_ARMOR:
     case TV_MAIL:
     {
-        /* Fall Through */
+        __attribute__((fallthrough));
     }
 
     /* Missiles & most things from above */
