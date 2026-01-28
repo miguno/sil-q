0e8cc74f4c8e3da81cd79a6c1b2623abd7a490cc
miguno <miguno@miguno.com>
2026-01-28 14:45:44 +0100

    Enable sound/audio mode with `-a`, change `-v` to print Sil-Q version
    This is a breaking change for the CLI `sil` binary.

--- src/main.c
+++ src/main.c
@@ -565,6 +565,7 @@ int main(int argc, char* argv[])
         {
             printf("%s version %s\n", VERSION_NAME, VERSION_STRING);
             quit(NULL);
+            break;
         }
 
         case '-':
